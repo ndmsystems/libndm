291b6d369e2916fb336df6290195179f55657f47
Alexander Papenko <ap@ndmsystems.com>
2012-06-25 18:30:25 +0400

    Added function ndmTime_get_monotonic_s

--- time.c
+++ time.c
@@ -9,6 +9,13 @@ long long ndmTime_get_monotonic_us()
 	return ts.tv_sec * 1000000ULL + ts.tv_nsec / 1000;
 }
 
+unsigned ndmTime_get_monotonic_s()
+{
+	struct timespec ts;
+
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return ts.tv_sec;
+}
 
 void ndmTime_get_time(struct timeval *t)
 {
