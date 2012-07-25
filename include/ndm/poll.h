89a3fa1037020d672376a76570e5f67cc5dfe977
Sergey Korolev <s.korolev@ndmsystems.com>
2012-07-25 12:02:18 +0400

    ndm_poll: attribute fix

--- include/ndm/poll.h
+++ include/ndm/poll.h
@@ -2,13 +2,13 @@
 #define __NDM_POLL_H__
 
 #include <poll.h>
-#include "macro.h"
+#include "attr.h"
 
 /* interruptible poll */
 int ndm_poll(
 		struct pollfd *fds,
 		const nfds_t nfds,
-		const int interval) NDM_WUR;
+		const int interval) NDM_ATTR_WUR;
 
 #endif	/* __NDM_POLL_H__ */
 
