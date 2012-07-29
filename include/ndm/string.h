cbd3b45374e0b0102a54178488cc2054d5b48ea5
Sergey Korolev <s.korolev@ndmsystems.com>
2012-07-29 22:28:51 +0400

    ndm_string_ndup() added

--- include/ndm/string.h
+++ include/ndm/string.h
@@ -3,7 +3,12 @@
 
 #include "attr.h"
 
-char *ndm_string_dup(const char *const s) NDM_ATTR_WUR;
+char *ndm_string_dup(
+		const char *const s) NDM_ATTR_WUR;
+
+char *ndm_string_ndup(
+		const char *const s,
+		const size_t size) NDM_ATTR_WUR;
 
 #endif	/* __NDM_STRING_H__ */
 
--- src/string.c
+++ src/string.c
@@ -13,3 +13,25 @@ char *ndm_string_dup(const char *const s)
 	return s_copy;
 }
 
+char *ndm_string_ndup(
+		const char *const value,
+		const size_t length)
+{
+	size_t l = 0;
+	char *val = NULL;
+
+	/* a value may not be a null-terminated string */
+	while (l < length && value[l] != '\0') {
+		++l;
+	}
+
+	val = malloc(l + 1);
+
+	if (val != NULL) {
+		memcpy(val, value, l);
+		val[l] = '\0';
+	}
+
+	return val;
+}
+
