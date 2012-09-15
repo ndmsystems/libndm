cd378a2ddd3580130a40d9b3ca9fd0e432e403d3
Sergey Korolev <s.korolev@ndmsystems.com>
2012-09-15 11:53:53 +0400

    macro.h: NDM_HEX_DUMP improvement

--- include/ndm/macro.h
+++ include/ndm/macro.h
@@ -3,6 +3,11 @@
 
 #include <stdint.h>
 #include <inttypes.h>
+#include "int.h"
+
+#ifndef NDEBUG
+#include <ctype.h>
+#endif
 
 #define NDM_ARRAY_SIZE(a)				(sizeof(a)/sizeof((a)[0]))
 
@@ -16,21 +21,42 @@
 enum { name = 1/(!!expr) }
 
 #ifndef NDEBUG
-#define NDM_HEX_DUMP(p, size)								\
-	do {													\
-		unsigned long __i__ = 0;							\
-															\
-		while (__i__ < size) {								\
-			if (__i__ % 16 == 0) {							\
-				printf("\n%08"PRIx32":", (uint32_t) __i__);	\
-			}												\
-															\
-			printf(" %02"PRIx8, ((uint8_t *) p)[__i__++]);	\
-		}													\
-															\
-		if ((__i__ - 1) % 16 != 0) {						\
-			printf("\n");									\
-		}													\
+#define NDM_HEX_DUMP_WIDTH				16
+#define NDM_HEX_DUMP(p, size)											\
+	do {																\
+		size_t __b__ = 0;												\
+																		\
+		while (__b__ < (size_t) size) {									\
+			size_t __i__ = 0;											\
+			size_t __e__ = (size_t)										\
+				NDM_MIN(NDM_HEX_DUMP_WIDTH, ((size_t) size) - __b__);	\
+																		\
+			printf("\n%08"PRIx32":", (uint32_t) __b__);					\
+																		\
+			while (__i__ < __e__) {										\
+				printf(" %02"PRIx8, ((uint8_t *) p)[__b__ + __i__]);	\
+				++__i__;												\
+			}															\
+																		\
+			while (__i__ < NDM_HEX_DUMP_WIDTH) {						\
+				printf("   ");											\
+				++__i__;												\
+			}															\
+																		\
+			printf(" | ");												\
+			__i__ = 0;													\
+																		\
+			while (__i__ < __e__) {										\
+				const char __c__ = ((char *) p)[__b__ + __i__];			\
+																		\
+				printf("%c", isprint(__c__) ? __c__ : '.');				\
+				++__i__;												\
+			}															\
+																		\
+			__b__ += NDM_HEX_DUMP_WIDTH;								\
+		}																\
+																		\
+		printf("\n");													\
 	} while (0)
 #else	/* NDEBUG */
 #define NDM_HEX_DUMP(p, size)
