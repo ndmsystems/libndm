b4cb0f3c91de5dba2012c4c3bfdd73c4c524aa64
Sergey Korolev <s.korolev@ndmsystems.com>
2012-12-11 19:17:25 +0400

    stdio.h: NDM_ATTR_PRINTF added

--- include/ndm/stdio.h
+++ include/ndm/stdio.h
@@ -2,30 +2,31 @@
 #define __NDM_STDIO_H__
 
 #include <stdarg.h>
+#include "attr.h"
 
 int ndm_asprintf(
 		char **strp,
 		const char *const format,
-		...);
+		...) NDM_ATTR_PRINTF(2, 3);
 
 int ndm_vasprintf(
 		char **strp,
 		const char *const format,
-		va_list ap);
+		va_list ap) NDM_ATTR_PRINTF(2, 0);
 
 int ndm_absprintf(
 		char *buffer,
 		const size_t buffer_size,
 		char **strp,
 		const char *const format,
-		...);
+		...) NDM_ATTR_PRINTF(4, 5);
 
 int ndm_vabsprintf(
 		char *buffer,
 		const size_t buffer_size,
 		char **strp,
 		const char *const format,
-		va_list ap);
+		va_list ap)  NDM_ATTR_PRINTF(4, 0);
 
 #endif	/* __NDM_STDIO_H__ */
 
