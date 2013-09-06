093c4ec5807c50d85c8069a91f45bffe14ab3932
Julia Petrenko <petrenko@ndmsystems.com>
2013-09-06 14:08:18 +0400

    Descriptions to all objects were added

--- include/ndm/feedback.h
+++ include/ndm/feedback.h
@@ -5,12 +5,22 @@
 #include <stdbool.h>
 #include "attr.h"
 
+/**
+ * The character which is used to separate the environment variables
+ * in the format string when ndm_feedback() is calling. Now it is newline
+ * character.
+ */
+
 #define NDM_FEEDBACK_ENV_SEPARATOR		"\n"
+
+/**
+ * Default timeout for ndm_feedback() function call, in milliseconds.
+ */
 
 #define NDM_FEEDBACK_TIMEOUT_MSEC		15000	/* 15 sec.	*/
 
-/**
- * #define ESEP NDM_FEEDBACK_ENV_SEPARATOR
+/*
+ * define ESEP NDM_FEEDBACK_ENV_SEPARATOR
  *
  * const char args[] =
  * {
@@ -28,6 +38,25 @@
  * 		str_arg, int_arg))
  * {
  * 		...
+ */
+
+/**
+ * Calls the external application and checks the result of its launch.
+ * Application is a special program (or a symbolic link to it), which is able
+ * to send its arguments and environment variables to the NDM core.
+ *
+ * @param timeout_msec The maximum time allowed to run, in milliseconds.
+ * @param argv An array of arguments for the application to run. It must contain
+ * at least one entry with a pointer to the name of the caller, as well as
+ * an optional set of additional arguments. The list must be terminated with
+ * @c NULL.
+ * @param env_format String with a format that creates a list of environment
+ * variables. The format is a string (similar to the format string from a group
+ * of functions @b printf) separated by '=' and NDM_FEEDBACK_ENV_SEPARATOR
+ * characters.
+ *
+ * @returns @c true if launch and sending of arguments and environment variables
+ * are successful, @c false — otherwise, @a errno stores an error code.
  */
 
 bool ndm_feedback(
