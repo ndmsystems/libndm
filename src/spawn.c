519175d2e4d288f09924f1409cb0c87e654ee090
Sergey Korolev <s.korolev@ndmsystems.com>
2020-12-14 01:04:32 +0300

    spawn.c: call async-signal-safe functions only in a child, NDMS-1215
    Memory allocation functions as well as setenv() call
    are not async-signal-safe.

--- src/spawn.c
+++ src/spawn.c
@@ -4,13 +4,198 @@
 #include <signal.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <ndm/int.h>
+#include <ndm/stdio.h>
 #include <ndm/spawn.h>
 #include <ndm/string.h>
+#include <ndm/strmap.h>
+
+#define NDM_SPAWN_SLASH_								((char) '/')
+#define NDM_SPAWN_COLON_								((char) ':')
+#define NDM_SPAWN_EQL_									((char) '=')
+#define NDM_SPAWN_NIL_									((char) '\0')
+#define NDM_SPAWN_PTR_SIZE_								sizeof(void *)
+
+extern char **environ;
+
+static bool ndm_spawn_merge_env_(
+		struct ndm_strmap_t *map,
+		const char *const env[],
+		const bool check)
+{
+	size_t i = 0;
+
+	for (; env[i] != NULL; i++) {
+		const char *const n = env[i];
+		const char *const v = strchr(n, NDM_SPAWN_EQL_);
+
+		if (v == NULL) {
+			if (check) {
+				errno = EINVAL;
+				return false;
+			}
+
+			continue;
+		}
+
+		const size_t n_size = (size_t) (v - n);
+
+		if (!ndm_strmap_nset(map, n, n_size, v + 1, strlen(v + 1))) {
+			errno = ENOMEM;
+			return false;
+		}
+	}
+
+	return true;
+}
+
+static inline size_t ndm_spawn_env_str_size_(
+		const char *const name,
+		const char *const value)
+{
+	const size_t size =
+		strlen(name) + sizeof(NDM_SPAWN_EQL_) +
+		strlen(value) + sizeof(NDM_SPAWN_NIL_);
+
+	return NDM_INT_ALIGN(size, NDM_SPAWN_PTR_SIZE_);
+}
+
+static char **ndm_spawn_alloc_env_(const char *const envp[])
+{
+	int error = errno;
+	char **env = NULL;
+	struct ndm_strmap_t map = NDM_STRMAP_INITIALIZER_DEFAULT;
+
+	if (!ndm_spawn_merge_env_(&map, (const char *const *)environ, false)) {
+		error = errno;
+		goto failed;
+	}
+
+	if (!ndm_spawn_merge_env_(&map, envp, true)) {
+		error = errno;
+		goto failed;
+	}
+
+	const size_t env_count = ndm_strmap_size(&map);
+	size_t env_size = NDM_SPAWN_PTR_SIZE_;
+	size_t i = 0;
+
+	for (; i < env_count; i++) {
+		const char *const n = ndm_strmap_get_key(&map, i);
+		const char *const v = ndm_strmap_get_by_index(&map, i);
+
+		env_size += NDM_SPAWN_PTR_SIZE_ + ndm_spawn_env_str_size_(n, v);
+	}
+
+	env = malloc(env_size);
+
+	if (env == NULL) {
+		error = errno;
+		goto failed;
+	}
+
+	char *env_str = ((char *) env) + (env_count + 1) * NDM_SPAWN_PTR_SIZE_;
+
+	for (i = 0; i < env_count; i++) {
+		const char *const n = ndm_strmap_get_key(&map, i);
+		const char *const v = ndm_strmap_get_by_index(&map, i);
+		const size_t env_str_size = ndm_spawn_env_str_size_(n, v);
+
+		env[i] = env_str;
+		snprintf(env_str, env_str_size, "%s%c%s", n, NDM_SPAWN_EQL_, v);
+		env_str += env_str_size;
+	}
+
+	env[i] = NULL;
+
+failed:
+	ndm_strmap_clear(&map);
+	errno = error;
+
+	return env;
+}
+
+static char *ndm_spawn_get_executable__(
+		const char *const file_name,
+		const char *const path_start,
+		const char *const path_end)
+{
+	int error = errno;
+	char *exec = NULL;
+	const int ret = ndm_asprintf(&exec, "%.*s%c%s",
+		(int) (path_end - path_start), path_start,
+		NDM_SPAWN_SLASH_, file_name);
+
+	if (ret < 0) {
+		return NULL;
+	}
+
+	struct stat statbuf;
+
+	if (stat(exec, &statbuf) != 0) {
+		error = errno;
+		goto failed;
+	}
+
+	if (!S_ISREG(statbuf.st_mode)) {
+		/* not a regular file */
+		error = ENOENT;
+		goto failed;
+	}
+
+	if (access(exec, R_OK | X_OK) != 0) {
+		error = errno;
+		goto failed;
+	}
+
+	return exec;
+
+failed:
+	free(exec);
+	errno = error;
+
+	return NULL;
+}
+
+static char *ndm_spawn_get_executable_(const char *const file_name)
+{
+	if (strchr(file_name, NDM_SPAWN_SLASH_) != NULL) {
+		/* do not try to find in PATH directories */
+		return ndm_string_dup(file_name);
+	}
+
+	const char *const path = getenv("PATH");
+
+	if (path != NULL) {
+		const char *const path_end = path + strlen(path);
+		const char *start = path;
+
+		while (start < path_end) {
+			const char *const colon = strchr(start, NDM_SPAWN_COLON_);
+			const char *const end = (colon == NULL) ? path_end : colon;
+			char *exec = ndm_spawn_get_executable__(file_name, start, end);
+
+			if (exec != NULL) {
+				return exec;
+			}
+
+			if (errno != ENOENT) {
+				return NULL;
+			}
+
+			start = end + 1;
+		}
+	}
+
+	errno = ENOENT;
+
+	return NULL;
+}
 
 bool ndm_spawn_default_at_exec(
 		const char *const argv[],
@@ -68,8 +253,16 @@ pid_t ndm_spawn_process(
 		 * in a child process only async-signal-safe functions
 		 * can be used (see "man 7 signal" for a list) */
 		const int fd_max = (int) sysconf(_SC_OPEN_MAX) - 1;
+		char **env = ndm_spawn_alloc_env_(envp);
+		char *executable = ndm_spawn_get_executable_(argv[0]);
 		int flags;
 
+		if (env == NULL) {
+			/* unable to allocate a new environment variable set */
+		} else
+		if (executable == NULL) {
+			/* unable to find an executable to run */
+		} else
 		if (((flags = fcntl(fb_fd[1], F_GETFD)) == -1) ||
 			(fcntl(fb_fd[1], F_SETFD, flags | FD_CLOEXEC) == -1))
 		{
@@ -85,46 +278,12 @@ pid_t ndm_spawn_process(
 			pid = NDM_SPAWN_INVALID_PID;
 		} else
 		if (pid == 0) {
-			bool env_error = false;
-
-			if (envp != NULL) {
-				/* append new (or replace old) environment variables */
-				unsigned long k = 0;
-
-				while (envp[k] != NULL && !env_error) {
-					char *name = ndm_string_dup(envp[k]);
-
-					if (name == NULL) {
-						env_error = true;
-					} else {
-						char *value = strchr(name, '=');
-
-						if (value == NULL) {
-							/* no '=' symbol in a environment entry */
-							errno = EINVAL;
-							env_error = true;
-						} else {
-							*value = '\0';
-							++value;
-							env_error = (setenv(name, value, 1) != 0);
-						}
-
-						free(name);
-					}
-
-					++k;
-				}
-			}
-
-			if (env_error) {
-				/* failed to setup environment variables */
-			} else
 			if (at_exec != NULL &&
 				!at_exec(argv, envp, fd_max, fb_fd[1], user_data))
 			{
 				/* at_exec() returned an error in the "errno" variable */
 			} else {
-				execvp(argv[0], (char *const *)(&argv[0]));
+				execve(executable, (char *const *)(&argv[0]), env);
 				/* could not execute */
 			}
 
@@ -139,7 +298,7 @@ pid_t ndm_spawn_process(
 				left -= n;
 			}
 
-			/* terminate a child without a successfull execvp() call */
+			/* terminate a child without a successfull execve() call */
 			_exit(EXIT_FAILURE);
 		} else {
 			int error = 0;
@@ -175,6 +334,9 @@ pid_t ndm_spawn_process(
 				pid = NDM_SPAWN_INVALID_PID;
 			}
 		}
+
+		free(executable);
+		free(env);
 	}
 
 	return pid;
--- tests/test_feedback.c
+++ tests/test_feedback.c
@@ -6,7 +6,7 @@
 #include "test.h"
 
 #define ESEP_			NDM_FEEDBACK_ENV_SEPARATOR
-#define OUTPUT_			"ndm_feedback.o"
+#define OUTPUT_			"ndm_feedback.output"
 
 int main(int argc, char *argv[])
 {
@@ -34,7 +34,9 @@ int main(int argc, char *argv[])
 			fprintf(fp,
 				"%s=%s\n"
 				"%s=%s\n"
+				"%s=%s\n"
 				"%s=%s\n",
+				"env0", getenv("env0"),
 				"env1", getenv("env1"),
 				"env2", getenv("env2"),
 				"env3", getenv("env3"));
@@ -45,6 +47,9 @@ int main(int argc, char *argv[])
 	}
 
 	unlink(OUTPUT_);
+
+	NDM_TEST(setenv("env0", "global", 1) == 0);
+	NDM_TEST(setenv("env2", "global", 1) == 0);
 
 	feedback_done = ndm_feedback(
 			200, args,
@@ -62,6 +67,9 @@ int main(int argc, char *argv[])
 		FILE *fp = fopen(OUTPUT_, "r");
 
 		NDM_TEST_BREAK_IF(fp == NULL);
+
+		fgets(env, sizeof(env), fp);
+		NDM_TEST(strcmp(env, "env0=global\n") == 0);
 
 		fgets(env, sizeof(env), fp);
 		NDM_TEST(strcmp(env, "env1=val1\n") == 0);
